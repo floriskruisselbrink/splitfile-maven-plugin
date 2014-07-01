37aebdbb98c5404a119594a3d28d334296e72498
Floris Kruisselbrink <floris@vloris.nl>
2014-07-01 17:40:26 +0200

    Integration-test toegevoegd

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,12 @@
-File touchFile = new File( basedir, "target/touch.txt" );
+import org.apache.commons.io.FileUtils
 
-assert touchFile.isFile()
+File test1 = new File( basedir, "test-destination-1.txt");
+File test2 = new File( basedir, "test-destination-2.txt");
+File expected1 = new File( basedir, "test-expected-1.txt");
+File expected2 = new File( basedir, "test-expected-2.txt");
+
+assert test1.isFile()
+assert test2.isFile()
+
+assert FileUtils.contentEquals(test1, expected1)
+assert FileUtils.contentEquals(test2, expected2)
